58419fe4d9fc735792db51eee6e84bb8d948d6d5
joaquingutierrez <joaquinguty@gmail.com>
2024-07-02 19:15:02 -0300

    Agregada la condicion de que solo se pueden tomar vacaciones en el mismo rango de fechas los empleados con distintos roles

--- models/vacationSchema.js
+++ models/vacationSchema.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const vacationSchema = new Schema({
-    employeeId: { type: mongoose.Schema.Types.ObjectId, ref: 'id', required: true },
+    employeeId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    rolId: { type: mongoose.Schema.Types.ObjectId, required: true },
     startDate: Date,
     endDate: Date
 });
--- utils/index.js
+++ utils/index.js
@@ -1,14 +1,16 @@
 const validationDate = (startDate, endDate, dateList, condition) => {
+    /* Para que sea una fecha de vacaciones valida, debe estar fuera del rango de las demas fechas; y si esta dentro del rango, debe ser diferente rol */
     startDate = new Date(startDate)
     endDate = new Date(endDate)
     for (let range of dateList) {
         let rangeStartDate = new Date(range.startDate);
         let rangeEndDate = new Date(range.endDate);
 
-        if ((startDate >= rangeStartDate && startDate <= rangeEndDate) || 
+        if (((startDate >= rangeStartDate && startDate <= rangeEndDate) ||
             (endDate >= rangeStartDate && endDate <= rangeEndDate) ||
-            (startDate <= rangeStartDate && endDate >= rangeEndDate)) {
-            return false;
+            (startDate <= rangeStartDate && endDate >= rangeEndDate)) &&
+            condition(range.rolId)) {
+            return false
         }
     }
     return true;
